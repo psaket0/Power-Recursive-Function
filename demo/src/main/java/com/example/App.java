e0b47707a51739dcced81b1bd9a1b41f064c0fd0
Saket Pathak <99856959+psaket0@users.noreply.github.com>
2023-04-24 00:36:36 -0700

    test 4

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,4 +1,14 @@
-import java.util.Scanner;
+double result = calculatePower(x, n);
+System.out.printf("%.4f to the power of %d = %.4f", x, n, result);
+}
 
-public class RectangleArea {
-    public static void main(String[] args) {
+private static double calculatePower(double x, int n) {
+if (n == 0) {
+    return 1;
+} else if (n % 2 == 0) {
+    double halfPower = calculatePower(x, n / 2);
+    return halfPower * halfPower;
+} else {
+    double halfPower = calculatePower(x, (n - 1) / 2);
+    return x * halfPower * halfPower;
+}
