eeda5bb74514a1da58e11085a8e4b78ddb523410
Saket Pathak <99856959+psaket0@users.noreply.github.com>
2023-04-24 00:37:30 -0700

    test 5

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,3 +1,29 @@
- (n - 1) / 2);
-    return x * halfPower * halfPower;
+package com.example;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a non-negative whole number: ");
+        int n = Integer.parseInt(scanner.nextLine().trim());
+        System.out.print("Enter a floating point number: ");
+        double x = Double.parseDouble(scanner.nextLine().trim());
+
+        double result = calculatePower(x, n);
+        System.out.printf("%.4f to the power of %d = %.4f", x, n, result);
+    }
+
+    private static double calculatePower(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n % 2 == 0) {
+            double halfPower = calculatePower(x, n / 2);
+            return halfPower * halfPower;
+        } else {
+            double halfPower = calculatePower(x, (n - 1) / 2);
+            return x * halfPower * halfPower;
+        }
+    }
 }
