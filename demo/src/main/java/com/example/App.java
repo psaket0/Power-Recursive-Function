d6b83da7c3ccc8ef943f04c397b38d65bf434bbc
Saket Pathak <99856959+psaket0@users.noreply.github.com>
2023-04-24 00:36:54 -0700

    test 3

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,14 +1,3 @@
-double result = calculatePower(x, n);
-System.out.printf("%.4f to the power of %d = %.4f", x, n, result);
-}
-
-private static double calculatePower(double x, int n) {
-if (n == 0) {
-    return 1;
-} else if (n % 2 == 0) {
-    double halfPower = calculatePower(x, n / 2);
-    return halfPower * halfPower;
-} else {
-    double halfPower = calculatePower(x, (n - 1) / 2);
+ (n - 1) / 2);
     return x * halfPower * halfPower;
 }
